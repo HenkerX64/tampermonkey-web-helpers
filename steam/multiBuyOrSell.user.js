c537dd59d7e1db6c316ebedecd5542ad7d42fd79
GRandFather <HenkerX64@users.noreply.github.com>
2023-12-28 21:08:51 +0100

    Fixes multiBuyOrSell.user.js
    - Skip first 5 lines

--- steam/multiBuyOrSell.user.js
+++ steam/multiBuyOrSell.user.js
@@ -3,7 +3,7 @@
 // @description  -
 // @namespace    https://github.com/HenkerX64
 // @updateURL    https://raw.githubusercontent.com/HenkerX64/tampermonkey-web-helpers/main/steam/multiBuyOrSell.user.js
-// @version      0.2
+// @version      0.3
 // @description  Open dialog helper for multi buy/sell with price selection from detail page.
 // @author       Henkerx64
 // @match        *://steamcommunity.com/market/multisell*
@@ -98,10 +98,10 @@
                     $('#market_commodity_buyreqeusts_table').html( data.buy_order_table ).find('tr:last').remove();
                     var $tbody_forsale = $('#market_commodity_forsale_table>table>tbody');
                     var $tbody_forbuy = $('#market_commodity_buyreqeusts_table>table>tbody');
-                    for(const sell_graph of data.sell_order_graph) {
+                    for(const sell_graph of data.sell_order_graph.slice(5)) {
                         $tbody_forsale.append('<tr><td align=right>' +data.price_prefix+' '+swhX64.formatNum(sell_graph[0])+' ' +data.price_suffix+'</td><td align=right>'+sell_graph[1]+'</td></tr>');
                     }
-                    for(const buy_graph of data.buy_order_graph) {
+                    for(const buy_graph of data.buy_order_graph.slice(5)) {
                         $tbody_forbuy.append('<tr><td align=right>'+data.price_prefix+' '+swhX64.formatNum(buy_graph[0])+' ' +data.price_suffix+'</td><td align=right>'+buy_graph[1]+'</td></tr>');
                     }
                     var targetId = '#'+swhX64.multiAction+"_"+item_nameid+"_price"+(swhX64.multiAction=='sell'?'_paid':'');
